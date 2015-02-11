25326f7b220d22fd2390adfe17be33c0d265841a
Tyler Anderson <tanderson6@mail.usf.edu>
2015-02-10 22:50:34 -0500

    Changed the SupportClass to create a new method to make a Date instance. Main class extends SupportClass and calls the getDate method in SupportClass.

--- HW6-EGit/src/Main.java
+++ HW6-EGit/src/Main.java
@@ -1,9 +1,11 @@
 
-public class Main {
+public class Main extends SupportClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is the Main Class!");
+		System.out.print("Right now it is: ");
+		System.out.println(getDate());
 	}
 
 }
--- HW6-EGit/src/SupportClass.java
+++ HW6-EGit/src/SupportClass.java
@@ -1,4 +1,11 @@
+import java.util.Date;
 
 public class SupportClass {
 
+	
+	public static Date getDate() {
+		Date now = new Date();
+		return now;
+	}
+	
 }
